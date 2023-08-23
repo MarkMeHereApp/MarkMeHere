0d866df881f518c541add81ff3d982433bbafd82
Aldrich Agabin <ArcFlu52@gmail.com>
2023-08-22 23:26:30 -0400

    Revert "Revert "Aagabin/setup prisma db""
    This reverts commit ecb7adf3f433098234bf25756a60b8a3fec7de3a.

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import prisma from '@/prisma';
 import bcrypt from 'bcrypt';
+import prisma from '@/prisma';
 
 export async function POST(request: Request) {
   const formData = await request.formData();
