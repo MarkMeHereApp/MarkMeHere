ccf0d852e03d16d23b76a5622a87157c3c6b8177
Walshingtonn <nicholas@bytefederal.com>
2023-08-24 12:37:09 -0400

    Created components folder inside overview. This folder holds components specific to the overview page

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { CalendarDateRangePicker } from '@/components/general/date-range-picker';
 import DashboardView from './DashboardView';
 import { Metadata } from 'next';
-import { ModeToggle } from './theme-toggle';
+import { ModeToggle } from './components/theme-toggle';
 import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
 
