bb3c4eb9f45fba3b06d8259647acfe7f9beca77f
Walshingtonn <nicholas@bytefederal.com>
2023-08-24 18:09:57 -0400

    Attmepting to fix build issues

--- app/api/prisma/route.ts
+++ app/api/prisma/route.ts
@@ -23,6 +23,7 @@ function createRandomUser(): User {
     lecturesAttended: faker.number.int({ max: totalLectures }),
     totalLectures: totalLectures,
     password: faker.string.sample({ min: 10, max: 20 })
+    dateCreated: new Date(),
   } as User;
 }
 
--- app/dashboard/class-settings/appearance/components/appearance-form.tsx
+++ app/dashboard/class-settings/appearance/components/appearance-form.tsx
@@ -36,7 +36,7 @@ const defaultValues: Partial<AppearanceFormValues> = {
   theme: "light",
 }
 
-export default function AppearanceForm() {
+export function AppearanceForm() {
   const form = useForm<AppearanceFormValues>({
     resolver: zodResolver(appearanceFormSchema),
     defaultValues,
