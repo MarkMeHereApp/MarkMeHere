427262e7e00eeb63bf117c6be3e083c984f4e8bb
Aldrich Agabin <ArcFlu52@gmail.com>
2023-08-26 07:06:16 -0400

    Add More Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   // Basic Settings
-  printWidth: 80, // Maximum line width
-  tabWidth: 2, // Number of spaces per indentation level
-  useTabs: false, // Use spaces instead of tabs
-  semi: true, // Add semicolons at the end of statements
-  singleQuote: true, // Use single quotes instead of double quotes
-  trailingComma: 'es5', // Add trailing commas where valid in ES5
-  bracketSpacing: true, // Add spaces between object literals within brackets
-  arrowParens: 'avoid', // Omit parens when there's only one argument in arrow functions
-
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
   // Overrides
   overrides: [
     {
