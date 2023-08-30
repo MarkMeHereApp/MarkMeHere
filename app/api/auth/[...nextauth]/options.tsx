2b2e0efe4f6663f8a47856e4f0e06994cff201ce
ben <bewerner23@gmail.com>
2023-08-30 01:13:54 -0400

    Fixing Build issue?

--- app/api/auth/[...nextauth]/options.tsx
+++ app/api/auth/[...nextauth]/options.tsx
@@ -51,7 +51,7 @@ export const authOptions: NextAuthOptions = {
 
         //If email is found check if password is correct
         //If user exists and entered password matches hashed password
-        if (user && (await bcrypt.compare(password, user.password))) {
+        if (user && user.password && (await bcrypt.compare(password, user.password))) {
           // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
