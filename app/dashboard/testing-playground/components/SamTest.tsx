749648acb57e907824e54d86f8700dac31a2301c
Samdosi <87053736+Samdosi@users.noreply.github.com>
2023-09-03 18:56:05 -0400

    add import csv UI

--- app/dashboard/testing-playground/components/SamTest.tsx
+++ app/dashboard/testing-playground/components/SamTest.tsx
@@ -1,42 +1,83 @@
 'use client';
 import { useCourseContext } from '@/app/course-context';
-import { CourseMember } from '@prisma/client';
-import { Button } from '@/components/ui/button';
 import { trpc } from '@/app/_trpc/client';
-import React, { useEffect, useState } from 'react';
-
+import { Input } from '@/components/ui/input';
+import Papa from 'papaparse';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog';
+import { DataTable } from './table';
 const SamsTestPage = () => {
   const data = useCourseContext();
   const createManyCourseMembers =
     trpc.courseMember.createMultipleCourseMembers.useMutation();
+  const [tableColumns, setTableColumns] = useState<string[]>([]);
+  const [tableValues, setTableValues] = useState<string[][]>([]);
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
+  const [isImporting, setIsImporting] = useState(false);
 
-  const [isLoading, setIsLoading] = useState(false);
+  const [isDialogOpen, setDialogOpen] = useState(false);
 
+  const openDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+
+    if (selectedFile) {
+      Papa.parse<CSVData>(selectedFile, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+          const columnsArray: string[][] = [];
+          const valuesArray: string[][] = [];
+
+          results.data.forEach((d) => {
+            columnsArray.push(Object.keys(d));
+            valuesArray.push(Object.values(d));
+          });
+
+          setTableColumns(columnsArray[0]);
+          setTableValues(valuesArray);
+          setIsFileUploaded(true);
+        }
+      });
+    } else {
+      setIsFileUploaded(false);
+    }
+  };
   const handleButtonClick = async () => {
     if (data.selectedCourseId === null) return;
-    setIsLoading(true);
-    try {
-      const newMembers = await createManyCourseMembers.mutateAsync({
-        courseId: data.selectedCourseId,
-        courseMembers: [
-          {
-            name: 'Test',
-            email: 'test@example.com',
-            role: 'student'
-          },
-          {
-            name: 'Test2',
-            email: 'test2@example.com',
-            role: 'student'
-          }
-        ]
-      });
-      data.setCourseMembersOfSelectedCourse(newMembers.allCourseMembersOfClass);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      throw new Error('Error Making Class Members');
-    }
+    const newMembers = await createManyCourseMembers.mutateAsync({
+      courseId: data.selectedCourseId,
+      courseMembers: [
+        {
+          name: 'Test',
+          email: 'test@example.com',
+          role: 'student'
+        },
+        {
+          name: 'Test2',
+          email: 'test2@example.com',
+          role: 'student'
+        }
+      ]
+    });
+
+    data.setCourseMembersOfSelectedCourse(newMembers.allCourseMembersOfClass);
   };
 
   return (
@@ -44,9 +85,31 @@ const SamsTestPage = () => {
       {Object.entries(data).map(([key, value]) => (
         <p key={key}>{`${key}: ${JSON.stringify(value)}`}</p>
       ))}
-      <Button onClick={handleButtonClick} disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'Add CourseMembers To List'}
-      </Button>
+      <Input id="csv" type="file" accept=".csv" onChange={handleFileChange} />
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button disabled={!isFileUploaded} onClick={openDialog}>
+            Preview
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[1000px]">
+          <DialogHeader>
+            <DialogTitle>Import CSV</DialogTitle>
+            <DialogDescription>
+              Review your CSV file here, click import when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <DataTable data={tableValues} courseID={data.selectedCourseId} />
+          </div>
+          <DialogFooter>
+            <Button type="button" variant="secondary" onClick={closeDialog}>
+              Cancel
+            </Button>
+            <Button type="submit">Import</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
---
+++ app/dashboard/testing-playground/components/table.tsx
@@ -0,0 +1,264 @@
+'use client';
+
+import * as React from 'react';
+import {
+  CaretSortIcon,
+  ChevronDownIcon,
+  DotsHorizontalIcon
+} from '@radix-ui/react-icons';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable
+} from '@tanstack/react-table';
+import { useCourseContext } from '@/app/course-context';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table';
+import { CourseMember } from '@prisma/client';
+
+let data = [];
+
+export const createColumns: (useCourse: any) => ColumnDef<CourseMember>[] = (
+  useCourse
+) => {
+  return [
+    {
+      accessorKey: 'Canvas ID',
+      cell: ({ row }) => <div className="capitalize">{row.original[0]}</div>
+    },
+    {
+      accessorKey: 'name',
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            Name
+            <CaretSortIcon className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => {
+        return <div className="font-medium">{row.original[1]}</div>;
+      }
+    },
+    {
+      accessorKey: 'email',
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            Email
+            <CaretSortIcon className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+      cell: ({ row }) => <div className="lowercase">{row.original[2]}</div>
+    },
+    {
+      accessorKey: 'courseID',
+      header: 'Course ID',
+      cell: ({ row }) => (
+        <div className="capitalize">{useCourse.selectedCourseId}</div>
+      )
+    },
+    {
+      id: 'actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        const payment = row.original;
+
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuItem
+                onClick={() => navigator.clipboard.writeText(payment.id)}
+              >
+                Copy payment ID
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>View customer</DropdownMenuItem>
+              <DropdownMenuItem>View payment details</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      }
+    }
+  ];
+};
+
+export function DataTable(props: { data: CourseMember[] }) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const useCourse = useCourseContext();
+  data = props.data;
+  const columns = createColumns(useCourse);
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection
+    }
+  });
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Find student..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('name')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
