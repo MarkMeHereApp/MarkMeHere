a3eda0ad57fc2e492ad3a511c4fa30e0875f9015
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-08 02:05:37 -0400

    Create Lecture Works but data table needs to re-render

--- app/dashboard/take-attendance/components/CreateNewLectureButton.tsx
+++ app/dashboard/take-attendance/components/CreateNewLectureButton.tsx
@@ -1,22 +1,24 @@
-'use client'
+'use client';
 
 import { Button } from '@/components/ui/button';
-import createNewLecture from '@/components/devUtils/createNewLecture';
 import { useCourseContext } from '@/app/course-context';
+import { trpc } from '@/app/_trpc/client';
+import { toast } from '@/components/ui/use-toast';
 
 export const CreateNewLectureButton = () => {
-    const { selectedCourseId , selectedAttendanceDate} = useCourseContext();
+  const { selectedCourseId, selectedAttendanceDate } = useCourseContext();
+  const createNewLectureMutation = trpc.lecture.CreateLecture.useMutation();
 
-    const handleClick = async () => {
-        if (selectedCourseId && selectedAttendanceDate) {
-            await createNewLecture(selectedAttendanceDate, selectedCourseId);
-        }
-    }   
-    return (
-        <Button onClick={() => handleClick()}>
-            Create a new lecture
-        </Button>
-    );
-}; // daddy leinecker please love our code <3 <3 <3
-
-// booooo analytics
+  const handleClick = async () => {
+    if (selectedCourseId && selectedAttendanceDate) {
+      await createNewLectureMutation.mutateAsync({
+        courseId: selectedCourseId || '',
+        lectureDate: selectedAttendanceDate || new Date()
+      });
+      toast({
+        title: `Successfully created a new lecture for ${selectedAttendanceDate}`
+      });
+    }
+  };
+  return <Button onClick={() => handleClick()}>Create a new lecture</Button>;
+};
