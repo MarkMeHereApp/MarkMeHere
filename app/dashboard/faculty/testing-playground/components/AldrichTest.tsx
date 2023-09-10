5995f5e75d921c8155fb54d42dc30f32a65545b4
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-10 04:36:46 -0400

    Fix Global Styles

--- app/dashboard/faculty/testing-playground/components/AldrichTest.tsx
+++ app/dashboard/faculty/testing-playground/components/AldrichTest.tsx
@@ -1,13 +1,11 @@
 'use client';
 import { useCourseContext } from '@/app/course-context';
-import CRUDButtons from '@/components/devUtils/CRUDButtons';
 
 const AldrichTestPage = () => {
   const data = useCourseContext();
 
   return (
     <div>
-      <CRUDButtons />
       {Object.entries(data).map(([key, value]) => (
         <p key={key}>{`${key}: ${JSON.stringify(value)}`}</p>
       ))}
