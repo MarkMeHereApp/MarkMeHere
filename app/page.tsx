c2d927733a3c33008eb267b957ac1278ea5f4c3a
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-10 19:27:44 -0400

    Replaced Loading Page State

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,50 @@ import { Icons } from '@/components/ui/icons';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
+const LoadingState = () => {
+  return (
+    <div className="pt-8 flex flex-col items-center justify-center">
+      <div className="flex space-x-0 items-center">
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '25px', height: 'auto' }}
+        />
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '50px', height: 'auto' }}
+        />
+
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '100px', height: 'auto' }}
+        />
+
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '150px', height: 'auto' }}
+        />
+
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '100px', height: 'auto' }}
+        />
+
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '50px', height: 'auto' }}
+        />
+        <Icons.logo
+          className="wave-infinite primary-foreground"
+          style={{ width: '25px', height: 'auto' }}
+        />
+      </div>
+      <h2 className="text-3xl italic font-bold tracking-tight">
+        "Mark Me Here!"
+      </h2>
+    </div>
+  );
+};
+
 export default function HomePage() {
   const router = useRouter();
 
@@ -22,10 +66,7 @@ export default function HomePage() {
         height: '100vh'
       }}
     >
-      <Icons.logo
-        className="wave primary-foreground"
-        style={{ height: 'auto', width: '150px' }}
-      />
+      <LoadingState />
     </div>
   );
 }
