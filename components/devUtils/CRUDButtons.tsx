a26f82f193bf4217554c1b96b220ae62d99a4dba
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-10 05:32:17 -0400

    Implement npm run prod

--- components/devUtils/CRUDButtons.tsx
+++ components/devUtils/CRUDButtons.tsx
@@ -17,7 +17,9 @@ const CRUDButtons = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <div className="border border-yellow-500 p-4 my-4">
         <p className="text-sm mb-4">
-          These buttons only show up in development mode
+          These buttons only show up in development mode. Run
+          <code> npm run prod </code>
+          instead to view the production version.
         </p>
         <div className="flex flex-row space-x-4">
           <EnrollCourseMemberButton />
