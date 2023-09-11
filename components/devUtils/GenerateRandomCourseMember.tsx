7ed8dbbac33b2b4a0e5b93b39b494dfbf04cfb9c
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-11 02:20:41 -0400

    Re-added Faker

--- components/devUtils/GenerateRandomCourseMember.tsx
+++ components/devUtils/GenerateRandomCourseMember.tsx
@@ -3,20 +3,21 @@
 import { useCourseContext } from '@/app/course-context';
 import { Button } from 'components/ui/button';
 import { CourseMember } from '@prisma/client';
-import { v4 as uuidv4 } from 'uuid';
+import { faker } from '@faker-js/faker';
 import { trpc } from '@/app/_trpc/client';
 import Loading from '../general/loading';
 import { zCourseMember } from '@/server/routes/courseMember';
 
-const createRandomCourseMember = (selectedCourseId: string): CourseMember => ({
-  id: uuidv4(),
-  lmsId: uuidv4(),
-  email: `${uuidv4()}@example.com`, // Generate a unique email
-  name: `User ${Math.floor(Math.random() * 1000)}`, // Generate a random name
-  courseId: selectedCourseId,
-  dateEnrolled: new Date(),
-  role: 'student'
-});
+const createRandomCourseMember = (selectedCourseId: string) =>
+  ({
+    id: faker.string.uuid(),
+    lmsId: faker.string.uuid(),
+    email: faker.internet.email(),
+    name: faker.person.fullName(),
+    courseId: selectedCourseId,
+    dateEnrolled: new Date(),
+    role: 'student'
+  }) as CourseMember;
 
 const GenerateRandomCourseMember = () => {
   const { selectedCourseId, setCourseMembersOfSelectedCourse } =
