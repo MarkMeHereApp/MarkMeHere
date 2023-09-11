1cb1a9f1b8a0a6d41c1b5699108d3804cccc54d7
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-10 20:00:28 -0400

    Added Logo to link to dashboard

--- app/dashboard/components/main-bar.tsx
+++ app/dashboard/components/main-bar.tsx
@@ -4,20 +4,26 @@ import { ModeToggle } from '@/app/dashboard/components/theme-toggle';
 import UserNav from '@/app/dashboard/components/user-nav';
 import MainNav from '@/app/dashboard/components/main-nav';
 import CourseSelection from './course-selection'; // Import the new component
+import Link from 'next/link';
+import { Icons } from '@/components/ui/icons';
 
 export default function MainBar() {
   return (
     <div className="border-b flex-col">
-      <div className="flex items-center mt-4 mb-4 pr-8 justify-between">
-        <div className="ml-4">
+      <div className="flex items-center mt-2 pb-4 pr-8 justify-between">
+        <div className="flex flex-row items-center ml-6 space-x-2">
+          <Link href="/dashboard/faculty/overview" className="-mr-2">
+            <Icons.logo style={{ flex: 1, width: '50px', height: '50px' }} />
+          </Link>
+          <Icons.slash style={{ width: '50px', height: '50px' }} />
           <CourseSelection />
         </div>
-        <div className="flex align-top items-center justify-end space-x-4 mt-0 p-0">
+        <div className="flex align-top items-center justify-end space-x-6 mt-0 p-0">
           <ModeToggle />
           <UserNav />
         </div>
       </div>
-      <div className="flex px-4">
+      <div className="flex px-6">
         <MainNav />
       </div>
     </div>
