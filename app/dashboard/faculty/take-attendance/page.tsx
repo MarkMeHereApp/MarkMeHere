ac8920dac46ebf773c673d9360502e0862054983
ben <bewerner23@gmail.com>
2023-09-12 19:04:30 -0400

    generate QR code only exists if there is a lecture

--- app/dashboard/faculty/take-attendance/page.tsx
+++ app/dashboard/faculty/take-attendance/page.tsx
@@ -6,16 +6,28 @@ import { DataTable } from './components/data-table';
 import { StartScanningButton } from './components/user-nav';
 import { useCourseContext } from '@/app/course-context';
 import CreateChooseCourseAnimation from '@/components/mark-me-here/CreateChooseCourseAnimation';
+import { useLecturesContext } from '@/app/dashboard/faculty/lecture-context';
 
 export default function ManageAttendance() {
-  const { selectedCourseId } = useCourseContext();
+  const { selectedCourseId, selectedAttendanceDate } = useCourseContext();
+  const { lectures } = useLecturesContext();
+
+  const getCurrentLecture = () => {
+    if (lectures && selectedAttendanceDate) {
+      return lectures.find((lecture) => {
+        return (
+          lecture.lectureDate.getTime() === selectedAttendanceDate.getTime()
+        );
+      });
+    }
+  };
 
   return (
     <div className="flex flex-col md:flex-row">
       <div className="block h-full flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Take Attendance</h2>
-          {selectedCourseId && <StartScanningButton />}
+          {getCurrentLecture() && <StartScanningButton />}
         </div>
         {selectedCourseId ? (
           <>
