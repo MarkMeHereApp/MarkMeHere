054fabc648620dd8bd8035423b09745fba9dbddd
ben <bewerner23@gmail.com>
2023-09-13 05:22:58 -0400

    disabling github for demo

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -24,7 +24,7 @@ const defaultProviders = [
   })
 ] as AuthOptions['providers'];
 
-if (isDevMode || true) {
+if (isDevMode) {
   defaultProviders.push(
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
