0d85b317d62c736b462c786183161b2dcd6e990d
Aldrich Agabin <ArcFlu52@gmail.com>
2023-09-15 16:19:23 -0400

    Implement isMobile Util

---
+++ utils/isMobile.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const isMobile = () => {
+  const [width, setWidth] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return window.innerWidth;
+    }
+    // Return a default value if window is not defined (for SSR)
+    return 0;
+  });
+
+  useEffect(() => {
+    const handleWindowSizeChange = () => {
+      setWidth(window.innerWidth);
+    };
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleWindowSizeChange);
+
+      return () => {
+        window.removeEventListener('resize', handleWindowSizeChange);
+      };
+    }
+  }, []);
+
+  return width <= 768;
+};
+
+export default isMobile;
