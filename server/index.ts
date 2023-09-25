5fec1de08e3faf1661151f9a6b5a19dbcead823e
jadynLP <jadynlapace@gmail.com>
2023-09-25 01:01:28 -0400

    delete all unused tRPC routes

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import { router } from './trpc';
 
 import { courseRouter } from './routes/course';
 import { courseMemberRouter } from './routes/courseMember';
-import { userRouter } from './routes/user';
 import { qrRouter } from './routes/qr';
 import { recordQRAttendanceRouter } from './routes/recordQRAttendance';
 import { lectureRouter } from './routes/lecture';
@@ -13,7 +12,6 @@ import { utilsRouter } from './routes/utils';
 export const appRouter = router({
   course: courseRouter,
   courseMember: courseMemberRouter,
-  user: userRouter,
   qr: qrRouter,
   recordQRAttendance: recordQRAttendanceRouter,
   lecture: lectureRouter,
--- server/routes/lecture.ts
+++ server/routes/lecture.ts
@@ -8,67 +8,12 @@ export const zGetLecturesOfCourse = z.object({
   courseId: z.string()
 });
 
-export const zTryGetLectureFromDate = z.object({
-  courseId: z.string(),
-  lectureDate: z.date()
-});
-
 export const zCreateLecture = z.object({
   courseId: z.string(),
   lectureDate: z.date()
 });
 
 export const lectureRouter = router({
-  getLecturesofCourse: publicProcedure
-    .input(zGetLecturesOfCourse)
-    .query(async (requestData) => {
-      try {
-        const lectures = await prisma.lecture.findMany({
-          where: {
-            courseId: requestData.input.courseId
-          }
-        });
-        return { success: true, lectures };
-      } catch (error) {
-        throw generateTypedError(error as Error);
-      }
-    }),
-  // This function returns the lecture object, and if returnAttendance is true, it also returns the attendance entries for that lecture.
-  tryGetLectureFromDate: publicProcedure
-    .input(zTryGetLectureFromDate)
-    .query(async (requestData) => {
-      try {
-        const lectures = await prisma.lecture.findMany({
-          where: {
-            courseId: requestData.input.courseId,
-            lectureDate: requestData.input.lectureDate
-          }
-        });
-
-        if (lectures.length === 0) {
-          return { success: false };
-        }
-
-        if (lectures.length > 1) {
-          throw new Error(
-            'More than one lecture found with the same date. This should never happen'
-          );
-        }
-
-        try {
-          const attendance = await prisma.attendanceEntry.findMany({
-            where: {
-              lectureId: lectures[0].id
-            }
-          });
-          return { success: true, lecture: lectures[0], attendance };
-        } catch (error) {
-          throw generateTypedError(error as Error);
-        }
-      } catch (error) {
-        throw generateTypedError(error as Error);
-      }
-    }),
   getAllLecturesAndAttendance: publicProcedure
     .input(zGetLecturesOfCourse)
 
