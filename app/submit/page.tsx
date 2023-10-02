c54db26c9f5a95dfe071d74a0ac322db8f591f03
Walshington <nicholaswalsh77@gmail.com>
2023-10-02 15:00:26 -0400

    TRPC caller initialized with context. Added request and resHeaders objects to solve type issues. receivedToken changed to const as it never changes

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -1,20 +1,17 @@
-import InputPage from "./components/inputPage"
-import { GetServerSidePropsContext } from "next";
-import prisma from "@/prisma";
+import InputPage from './components/inputPage';
+import { GetServerSidePropsContext } from 'next';
+import prisma from '@/prisma';
 import { v4 as uuidv4 } from 'uuid';
-import { redirect } from "next/navigation";
-import { replace } from "lodash";
-import { NextRequest, NextResponse } from "next/server";
-import { ur } from "@faker-js/faker";
-import { url } from "inspector";
-import { appRouter } from "@/server";
-import dynamic from "next/dynamic";
+import { redirect } from 'next/navigation';
+import { replace } from 'lodash';
+import { NextRequest, NextResponse } from 'next/server';
+import { ur } from '@faker-js/faker';
+import { url } from 'inspector';
+import { appRouter } from '@/server';
+import dynamic from 'next/dynamic';
+import { createContext } from '@/server/context';
 
-
-
-async function validateAndCreateToken(
-  qrCode: string,
-) {
+async function validateAndCreateToken(qrCode: string) {
   try {
     const qrResult = await prisma.qrcode.findUnique({
       where: {
@@ -29,77 +26,65 @@ async function validateAndCreateToken(
     const { id } = await prisma.attendanceToken.create({
       data: {
         lectureId: qrResult.lectureId,
-        token:  uuidv4()
+        token: uuidv4()
       }
     });
-        
-    return { success: true, token: id };
 
-    
-    
+    return { success: true, token: id };
   } catch (error) {
     throw error;
   }
-  
-
 }
 
-export default async function SubmitPage({searchParams}: {searchParams: any}) {
-  
-  const nextParamsCaller = appRouter.createCaller({});
+export default async function SubmitPage(
+  req: Request,
+  resHeaders: Headers,
+  { searchParams }: { searchParams: any }
+) {
+  const nextParamsCaller = appRouter.createCaller(
+    await createContext({ req, resHeaders })
+  );
 
   const handleToken = async () => {
-    
-    const res = await validateAndCreateToken(qrCode)
-  
-    if(res){
-      console.log(res)
+    const res = await validateAndCreateToken(qrCode);
+
+    if (res) {
+      console.log(res);
       return res.token;
+    } else {
     }
-    else{
+  };
 
-    }
-    
-  }
-  
-  console.log(searchParams)
-  let qrCode = ''
-  let error = ''
+  console.log(searchParams);
+  let qrCode = '';
+  let error = '';
 
-  if(searchParams.hasOwnProperty('qr')){
-    console.log("QR Param included")
+  if (searchParams.hasOwnProperty('qr')) {
+    console.log('QR Param included');
     qrCode = searchParams.qr;
-    
-    let receivedToken = await handleToken();
-    console.log('token out: ' + receivedToken)
 
-    if(receivedToken){
-      redirect(`/markAttendance?attendanceTokenId=${receivedToken}`)
-    }
+    const receivedToken = await handleToken();
+    console.log('token out: ' + receivedToken);
 
-    else{
-      redirect(`/submit`)//add error to the url and then retrieve it 
+    if (receivedToken) {
+      redirect(`/markAttendance?attendanceTokenId=${receivedToken}`);
+    } else {
+      redirect(`/submit`); //add error to the url and then retrieve it
     }
-    
   }
 
-  if(searchParams.hasOwnProperty('error')){
-    console.log("Error Param included")
-    error = searchParams.error
+  if (searchParams.hasOwnProperty('error')) {
+    console.log('Error Param included');
+    error = searchParams.error;
   }
-
-
-  
 
   return (
     <>
       <div className="relative min-h-screen">
-       
-
-        <div className='flex flex-col top-0 right-0 bottom-0 h-full w-full align-middle'>
+        <div className="flex flex-col top-0 right-0 bottom-0 h-full w-full align-middle">
           <InputPage></InputPage>
         </div>
       </div>
     </>
-  )
+  );
 }
