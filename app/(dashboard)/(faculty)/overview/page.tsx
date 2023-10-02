2cf3029f398fb36a36b53036fc17775ed43fb887
jadynLP <jadynlapace@gmail.com>
2023-10-02 16:21:41 -0400

    small change

--- app/(dashboard)/(faculty)/overview/page.tsx
+++ app/(dashboard)/(faculty)/overview/page.tsx
@@ -24,17 +24,14 @@ export default function Overview() {
     return (
       <div className="flex flex-col md:flex-row">
         <div className="block h-full w-full flex-1 flex-col space-y-4 p-8 md:flex">
-          <div className="flex flex-col">
-            <div className="flex flex-col items-center justify-center">
-              <div className="w-full h-full space-y-2">
-                {selectedCourseId && lectures && lectures.length > 0 ? (
-                  <OverviewAnalytics />
-                ) : (
-                  <NoLecture />
-                )}
-              </div>
-            </div>
-          </div>
+        <span className="text-3xl font-bold tracking-tight">
+          {`Welcome to ${selectedCourseName}, ${userName.substring(0, userName.indexOf(' '))}!`}
+        </span>
+        {selectedCourseId && lectures && lectures.length > 0 ? (
+          <OverviewAnalytics />
+        ) : (
+          <NoLecture />
+        )}
         </div>
       </div>
     );
