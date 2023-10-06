d097a879fdf091cc743f80e67d60bdd157d81304
Benjamin Werner <bewerner23@gmail.com>
2023-10-06 18:01:39 -0400

    Fixing Sign In Page with new provider settings
    - Fixed caching issue by using our database to determine which providers to display.
    - Displaying custom display names
    - Removed ugly provider type.

--- app/signin/components/signInForm.tsx
+++ app/signin/components/signInForm.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 
 import { Alert, AlertDescription } from '@/components/ui/alert';
-import { ClientSafeProvider, LiteralUnion } from 'next-auth/react';
 import { signIn, useSession } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
@@ -12,8 +11,6 @@ import dynamic from 'next/dynamic';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { firaSansFont } from '@/utils/fonts';
 
-// These are weird imports but it's how we can pass the getProviders type to the SignInForm component
-import { BuiltInProviderType } from 'next-auth/providers';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/ui/icons';
 import { Input } from '@/components/ui/input';
@@ -22,18 +19,11 @@ import { cn } from '@/lib/utils';
 import { useEffect } from 'react';
 import { toast } from '@/components/ui/use-toast';
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
-  providers: Record<
-    LiteralUnion<BuiltInProviderType, string>,
-    ClientSafeProvider
-  > | null;
+interface SignInFormProps {
+  providers: Array<{ key: string; displayName: string }>;
 }
 
-export default function SignInForm({
-  className,
-  providers,
-  ...props
-}: UserAuthFormProps) {
+export default function SignInForm({ providers }: SignInFormProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [isLoading, setIsLoading] = React.useState<{ [key: string]: boolean }>(
@@ -132,109 +122,99 @@ export default function SignInForm({
   };
 
   return (
-    <div className={cn('relative h-screen', className)} {...props}> 
+    <div className={cn('relative h-screen')}>
       <div className="absolute top-0 right-0 h-full w-full">
         <Stars />
       </div>
       <Card className="min-w-[300px] w-[25%] mx-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center p-4">
-        
-        
-        <CardContent className='flex flex-col w-[100%]'>
-
-          <CardTitle className='text-2xl font-bold font-mono text-center'>
+        <CardContent className="flex flex-col w-[100%]">
+          <CardTitle className="text-2xl font-bold font-mono text-center">
             <span className={firaSansFont.className}>
-                Sign in Mark Me Here!
+              Sign in Mark Me Here!
             </span>
           </CardTitle>
 
           <div className="flex flex-col gap-4">
-              
             <Alert className="text-center text-red-500 border-0">
               {error && (
-                  <AlertDescription>Log In Error: {error}</AlertDescription> 
+                <AlertDescription>Log In Error: {error}</AlertDescription>
               )}
-            </Alert>  
-            
+            </Alert>
 
             {providers &&
               Object.values(providers).some(
-                (provider) => provider.type === 'credentials'
+                (provider) => provider.key === 'credentials'
               ) && (
-                
-              <form onSubmit={onCredentialsSubmit}>
-                <div className="gap-2 space-y-5">
-                  
-                  <CardTitle className='text-center text-sm text-muted-foreground'>
-                    Enter your email & password below to login
-                  </CardTitle>
+                <form onSubmit={onCredentialsSubmit}>
+                  <div className="gap-2 space-y-5">
+                    <CardTitle className="text-center text-sm text-muted-foreground">
+                      Enter your email & password below to login
+                    </CardTitle>
 
-                  
-                  <div className="flex flex-col gap-1">
-                    <div className="text-sm font-bold mb-0">Email</div>
-                    
-                    <Label className="sr-only" htmlFor="email">
-                      Email
-                    </Label>
-                    
-                    <Input
-                      id="email"
-                      name="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="test@test is a valid email"
-                      type="email"
-                      autoCapitalize="none"
-                      autoComplete="email"
-                      autoCorrect="off"
-                      disabled={isLoading.credentials}
-                      required
-                      className="border flex"
-                    />
-                  </div>
+                    <div className="flex flex-col gap-1">
+                      <div className="text-sm font-bold mb-0">Email</div>
 
-                  <div className="flex flex-col gap-1">
-                    <div className="text-sm font-bold mb-0">Password</div>
-                    <Label className="sr-only" htmlFor="password">
-                      Password
-                    </Label>
-                    <Input
-                      id="password"
-                      name="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="test is a valid password"
-                      type="password"
-                      autoCapitalize="none"
-                      autoComplete="password"
-                      autoCorrect="off"
-                      disabled={isLoading.credentials}
-                      required
-                      className="border flex"
-                    />
+                      <Label className="sr-only" htmlFor="email">
+                        Email
+                      </Label>
+
+                      <Input
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="test@test is a valid email"
+                        type="email"
+                        autoCapitalize="none"
+                        autoComplete="email"
+                        autoCorrect="off"
+                        disabled={isLoading.credentials}
+                        required
+                        className="border flex"
+                      />
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                      <div className="text-sm font-bold mb-0">Password</div>
+                      <Label className="sr-only" htmlFor="password">
+                        Password
+                      </Label>
+                      <Input
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="test is a valid password"
+                        type="password"
+                        autoCapitalize="none"
+                        autoComplete="password"
+                        autoCorrect="off"
+                        disabled={isLoading.credentials}
+                        required
+                        className="border flex"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-1 mt-1">
+                      {' '}
+                      {/* Added a margin-top class to create a bigger gap */}
+                      <Button
+                        disabled={isLoading.credentials}
+                        variant="outline"
+                      >
+                        {isLoading.credentials && (
+                          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+                        Sign In
+                      </Button>
+                    </div>
                   </div>
-                  <div className="flex flex-col gap-1 mt-1">
-                    {' '}
-                    {/* Added a margin-top class to create a bigger gap */}
-                    <Button
-                      disabled={isLoading.credentials}
-                      variant="outline"
-                    >
-                      {isLoading.credentials && (
-                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                      )}
-                      Sign In
-                    </Button>
-                  </div>
-                </div>
-              </form>
+                </form>
               )}
-
-              
 
             {providers &&
               Object.values(providers).length > 1 &&
               Object.values(providers).some(
-                (provider) => provider.type === 'credentials'
+                (provider) => provider.key === 'credentials'
               ) && (
                 <div className="relative flex justify-center text-xs uppercase">
                   <span className="bg-transparent bars px-2 text-muted-foreground">
@@ -243,32 +223,29 @@ export default function SignInForm({
                 </div>
               )}
 
-              
-
             {providers &&
               Object.values(providers).map(
                 (provider, index) =>
-                  provider.type !== 'credentials' && (
+                  provider.key !== 'credentials' && (
                     <Button
                       key={index}
                       type="button"
-                      onClick={() => onOAuthSubmit(provider.id)}
-                      disabled={isLoading[provider.id]}
+                      onClick={() => onOAuthSubmit(provider.key)}
+                      disabled={isLoading[provider.key]}
                     >
-                      {isLoading[provider.id] ? (
+                      {isLoading[provider.key] ? (
                         <>
                           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
                           Loading...
                         </>
                       ) : (
-                        <>{provider.name}</>
+                        <>{provider.displayName}</>
                       )}
                     </Button>
                   )
               )}
           </div>
         </CardContent>
-        
       </Card>
     </div>
   );
