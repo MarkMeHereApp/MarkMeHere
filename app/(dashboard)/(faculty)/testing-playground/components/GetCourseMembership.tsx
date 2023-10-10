9a0e9a81b4ec6daa4a1adcf4e60c5c9f0710286f
Benjamin Werner <bewerner23@gmail.com>
2023-10-09 21:12:43 -0400

    Fixing testing-playground

--- app/(dashboard)/(faculty)/testing-playground/components/GetCourseMembership.tsx
+++ app/(dashboard)/(faculty)/testing-playground/components/GetCourseMembership.tsx
@@ -2,19 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useCourseContext } from '@/app/context-course';
 
 const GetCourseMembership = () => {
-  const { userCourseMembers } = useCourseContext();
-  const [courseMemberships, setCourseMemberships] = useState(null);
+  const { courseMembersOfSelectedCourse } = useCourseContext();
 
   return (
     <div>
-      {userCourseMembers &&
-        userCourseMembers.map((membership, index) => (
-          <div key={index}>
-            <p>Course ID: {membership.courseId}</p>
-            <p>Name: {membership.name}</p>
-            <p>Role: {membership.role}</p>
-          </div>
-        ))}
+      {courseMembersOfSelectedCourse &&
+        JSON.stringify(courseMembersOfSelectedCourse)}
     </div>
   );
 };
