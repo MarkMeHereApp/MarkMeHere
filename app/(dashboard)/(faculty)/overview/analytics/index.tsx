a4a9b365a351d3b8b4b6c25beac9e84a2caaa63d
Aldrich Agabin <ArcFlu52@gmail.com>
2023-10-22 00:38:48 -0400

    Implement ExportToJSON Button

--- app/(dashboard)/(faculty)/overview/analytics/index.tsx
+++ app/(dashboard)/(faculty)/overview/analytics/index.tsx
@@ -1,21 +1,33 @@
 import { useCourseContext } from '@/app/context-course';
 import { useLecturesContext } from '../../../../context-lecture';
 import AttendanceOverTimeLineGraph from './AttendanceOverTimeLineGraph';
+import OverviewBar from './OverviewBar';
 
 const OverviewAnalytics = () => {
   const { lectures } = useLecturesContext();
   // This gets the lectures from the current course already
   // lectures is an array of objects, where each object consists of a lecture and its attendance entries
 
-  const { courseMembersOfSelectedCourse } = useCourseContext();
+  const { courseMembersOfSelectedCourse, userCourses, selectedCourseId } =
+    useCourseContext();
   const studentsOfSelectedCourse = courseMembersOfSelectedCourse?.filter(
     (member) => member.role === 'student'
   );
+  const selectedCourseName = userCourses?.find((courses) => {
+    return courses.id === selectedCourseId;
+  })?.name;
   // After getting the data, pass it to the AttendanceOverTimeLineGraph component and let it handle the rest
   // Do the same for the top students
   return (
-    <div className="flex h-full w-full">
-      <div className="w-full h-full p-4">
+    <div className="flex h-full w-full flex-col">
+      <div className="w-full h-1/4 p-4">
+        <OverviewBar
+          selectedCourseName={selectedCourseName}
+          lectures={lectures}
+          courseMembers={courseMembersOfSelectedCourse}
+        />
+      </div>
+      <div className="w-full h-3/4 p-4">
         <AttendanceOverTimeLineGraph
           lectures={lectures}
           numStudents={studentsOfSelectedCourse?.length ?? 0}
