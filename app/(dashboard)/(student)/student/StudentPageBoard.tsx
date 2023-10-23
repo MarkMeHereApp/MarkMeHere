9e816c128183f360b35bf58ca1ffaa9d19a0f94f
jadynLP <jadynlapace@gmail.com>
2023-10-23 11:42:58 -0400

    memoized student attendance data

--- app/(dashboard)/(student)/student/StudentPageBoard.tsx
+++ app/(dashboard)/(student)/student/StudentPageBoard.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import * as React from 'react';
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useSearchParams } from 'next/navigation';
 import { useCourseContext } from '@/app/context-course';
 import { useSession } from 'next-auth/react';
-
 import { CourseMember } from "@prisma/client";
 import MarkAttendanceSuccess from '@/app/(dashboard)/(student)/markAttendance/components/mark-attendance-success';
 import StudentAttendanceEntriesTable from './components/StudentAttendanceEntriesTable';
@@ -21,34 +20,92 @@ interface StudentPageBoardProp {
 }
 
 const StudentPageBoard: React.FC<StudentPageBoardProp> = ({dateMarked}) => {
-  const session = useSession();
-  const userName = session?.data?.user?.name || '';
-  const userEmail = session.data?.user?.email;
-  const { courseMembersOfSelectedCourse } = useCourseContext();
-//   const [student, setStudent] = React.useState<CourseMember>();
-  const [showSuccess, setShowSuccess] = React.useState(true);
+    const session = useSession();
+    const userName = session?.data?.user?.name || '';
+    const userEmail = session.data?.user?.email;
+    const [showSuccess, setShowSuccess] = React.useState(true);
+    const [attendanceData, setAttendanceData] = useState<any[]>([]);
+    const { courseMembersOfSelectedCourse } = useCourseContext();
+    const { lectures } = useLecturesContext();  
 
     // for future use 
     //   const searchParams = useSearchParams();
     //   const attendanceEntry = searchParams ? searchParams.get('attendanceEntry') : null; //storing the searchParams with 'error' included, that is then being used the in the UseEffect below
-
-    const [isLoading, setIsLoading] = React.useState<boolean>(true);
-    const [attendanceData, setAttendanceData] = useState<any[]>([]);
-    const { lectures } = useLecturesContext();  
     
     const getCourseMember = () => {
         if (courseMembersOfSelectedCourse) {
           const selectedCourseMember: CourseMember | undefined = courseMembersOfSelectedCourse.find(
             (member) => member.email === userEmail
           );
-          if (selectedCourseMember) {
-            
-            // setStudent(selectedCourseMember);
+        if (selectedCourseMember) {
             return selectedCourseMember;
-          }
-          return null;
+        }
+        return null;
         }
     }
+
+    const calculateAttendanceEntryData = (attendanceEntries: AttendanceEntry[]) => {
+        const totalAttendanceEntries = attendanceEntries.length;
+        // here (green), absent (red), excused (yellow), late (orange)
+        const colors = ['#FBCE13', '#7F1D1D', '#ffc425', '#f37736'];
+
+        const hereEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
+            return entry.status === 'here';
+        });
+        const excusedEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
+            return entry.status === 'excused';
+        });
+        const lateEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
+            return entry.status === 'late';
+        });
+        const absentEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
+            return entry.status === 'absent';
+        });
+
+        const pieData1 = [
+            {
+                label: 'Attended',
+                name: 'Attended',
+                value:
+                ((hereEntries.length + excusedEntries.length + lateEntries.length) / totalAttendanceEntries) * 100,
+                fill: colors[0]
+            },
+            {
+                label: 'Not Attended',
+                name: 'Not Attended',
+                value: (absentEntries.length / totalAttendanceEntries) * 100,
+                fill: colors[1]
+            },
+        ];
+
+        const pieData2 = [
+            {
+                label: 'Here',
+                name: 'Here',
+                value: Math.round((hereEntries.length / totalAttendanceEntries) * 100),
+                fill: colors[0]
+            },
+            {
+                label: 'Excused',
+                name: 'Excused',
+                value: Math.round((excusedEntries.length / totalAttendanceEntries) * 100),
+                fill: colors[2]
+            },
+            {
+                label: 'Late',
+                name: 'Late',
+                value: Math.round((lateEntries.length / totalAttendanceEntries) * 100),
+                fill: colors[3]
+            },
+            {
+                label: 'Absent',
+                name: 'Absent',
+                value: Math.round((absentEntries.length / totalAttendanceEntries) * 100),
+                fill: colors[1]
+            }
+        ];
+        setAttendanceData([pieData1, pieData2]);
+    };
 
     const getAttendanceEntries = () => {
         const attendanceEntries: AttendanceEntry[] = [];
@@ -60,79 +117,12 @@ const StudentPageBoard: React.FC<StudentPageBoardProp> = ({dateMarked}) => {
                 }
             });
         });
-
+        calculateAttendanceEntryData(attendanceEntries);
         return attendanceEntries;
     }
 
-    useEffect(() => {
-        const calculateAttendanceEntryData = () => {
-          const attendanceEntries = getAttendanceEntries(); 
-          const totalAttendanceEntries = attendanceEntries.length;
-          // here (green), absent (red), excused (yellow), late (orange)
-          const colors = ['#FBCE13', '#7F1D1D', '#ffc425', '#f37736'];
-    
-          const hereEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
-            return entry.status === 'here';
-          });
-          const excusedEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
-            return entry.status === 'excused';
-          });
-          const lateEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
-            return entry.status === 'late';
-          });
-          const absentEntries = attendanceEntries.filter((entry: AttendanceEntry) => {
-            return entry.status === 'absent';
-          });
-    
-          const pieData1 = [
-            {
-              label: 'Attended',
-              name: 'Attended',
-              value:
-                ((hereEntries.length + excusedEntries.length + lateEntries.length) / totalAttendanceEntries) * 100,
-              fill: colors[0]
-            },
-            {
-              label: 'Not Attended',
-              name: 'Not Attended',
-              value: (absentEntries.length / totalAttendanceEntries) * 100,
-              fill: colors[1]
-            },
-          ];
-    
-          const pieData2 = [
-            {
-              label: 'Here',
-              name: 'Here',
-              value: Math.round((hereEntries.length / totalAttendanceEntries) * 100),
-              fill: colors[0]
-            },
-            {
-              label: 'Excused',
-              name: 'Excused',
-              value: Math.round((excusedEntries.length / totalAttendanceEntries) * 100),
-              fill: colors[2]
-            },
-            {
-              label: 'Late',
-              name: 'Late',
-              value: Math.round((lateEntries.length / totalAttendanceEntries) * 100),
-              fill: colors[3]
-            },
-            {
-              label: 'Absent',
-              name: 'Absent',
-              value: Math.round((absentEntries.length / totalAttendanceEntries) * 100),
-              fill: colors[1]
-            }
-        ];
-    
-          setAttendanceData([pieData1, pieData2]);
-        };
-        calculateAttendanceEntryData();
-      }, [lectures]);
+    const attendanceEntries = useMemo(() => getAttendanceEntries(), [lectures]);
 
-    
     useEffect(() => {
         if (dateMarked) {
           // Set a timeout to hide the success message after 5 seconds
@@ -155,7 +145,7 @@ const StudentPageBoard: React.FC<StudentPageBoardProp> = ({dateMarked}) => {
         {showSuccess && dateMarked ? (
             <MarkAttendanceSuccess dateMarked={dateMarked} />
         ) : (
-            getAttendanceEntries() && (
+            userName && attendanceEntries && (
             <>
                 <span className="text-3xl font-bold tracking-tight">
                 {`Welcome ${userName.substring(0, userName.indexOf(' '))}!`}
@@ -167,7 +157,7 @@ const StudentPageBoard: React.FC<StudentPageBoardProp> = ({dateMarked}) => {
                         style={{ height: '100px', width: '100px' }}
                     />
                 </div>
-                ) : getAttendanceEntries()?.length > 0 ? (
+                ) : attendanceEntries.length > 0 ? (
                     <>
                     <div className='flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0 w-full'>
                         <div className='flex flex-col space-y-4 md:w-3/4'>
@@ -175,7 +165,7 @@ const StudentPageBoard: React.FC<StudentPageBoardProp> = ({dateMarked}) => {
                             <StudentPieChart pieChartData={attendanceData[0]} />
                         </div>
                         <div className='w-full md:w-1/4'>
-                            <StudentAttendanceEntriesTable attendanceEntries={getAttendanceEntries()} />
+                            <StudentAttendanceEntriesTable attendanceEntries={attendanceEntries} />
                          </div>
                     </div>
                     </>
