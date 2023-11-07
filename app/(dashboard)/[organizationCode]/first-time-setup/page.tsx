9033a88f2738652b9ab3aa07b1781ff5c2aee84b
Benjamin Werner <bewerner23@gmail.com>
2023-11-07 14:33:18 -0500

    Fixed First Time Setup Bugs
    - Fixed Github/GithubEdu DocsLink
    - Redirected based on user status in the first-time-setup, instead of relying on the callback url

--- app/(dashboard)/[organizationCode]/first-time-setup/page.tsx
+++ app/(dashboard)/[organizationCode]/first-time-setup/page.tsx
@@ -1,9 +1,33 @@
 import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { getAuthOptions } from '@/app/api/auth/[...nextauth]/options';
+import prisma from '@/prisma';
+import { FirstTimeSteps } from './[step]/components/first-time-steps';
 
 export default async function Page({
   params
 }: {
   params: { organizationCode: string };
 }) {
+  const authOptions = await getAuthOptions();
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    throw new Error('No Valid Session Found');
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      email: session.user.email
+    }
+  });
+
+  if (user) {
+    redirect(
+      `/${params.organizationCode}/first-time-setup/${(
+        FirstTimeSteps.length - 1
+      ).toString()}`
+    );
+  }
+
   redirect(`/${params.organizationCode}/first-time-setup/0`);
 }
