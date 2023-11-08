a73238717a71209cc414a7fb8f76f6cf657280f1
Benjamin Werner <bewerner23@gmail.com>
2023-11-08 02:41:42 -0500

    Made links dependent on role

--- app/(dashboard)/[organizationCode]/[courseCode]/components/main-bar.tsx
+++ app/(dashboard)/[organizationCode]/[courseCode]/components/main-bar.tsx
@@ -12,12 +12,14 @@ import { useOrganizationContext } from '../../context-organization';
 import { useSession } from 'next-auth/react';
 import isDevMode from '@/utils/isDevMode';
 import { MobileNav } from './mobile-nav';
+import { zCourseRoles, zSiteRoles } from '@/types/sharedZodTypes';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function MainBar() {
   const session = useSession();
   const { organizationUrl } = useOrganizationContext();
   const pathname = usePathname();
-  const { currentCourseUrl } = useCourseContext();
+  const { currentCourseUrl, selectedCourseEnrollment } = useCourseContext();
   const isQRCodePage = usePathname() === `${currentCourseUrl}/qr`;
   const isVerificationPage =
     usePathname() === `${currentCourseUrl}/verification`;
@@ -67,24 +69,41 @@ export default function MainBar() {
     </Link>
   );
 
+  const CanAccessStudent = () => {
+    return selectedCourseEnrollment?.role === zCourseRoles.enum.student;
+  };
+
+  const CanAccessTeacher = () => {
+    if (session?.data?.user?.role === zSiteRoles.enum.admin) return true;
+    return selectedCourseEnrollment?.role === zCourseRoles.enum.teacher;
+  };
+
   const NavComponents = [
-    <MainNavBarCustomLink
-      href={`${currentCourseUrl}/student`}
-      displayText="Student"
-    />,
-    <MainNavBarCustomLink
-      href={`${currentCourseUrl}/overview`}
-      displayText="Overview"
-    />,
-    <MainNavBarCustomLink
-      href={`${currentCourseUrl}/attendance`}
-      displayText="Attendance"
-    />,
-    <MainNavBarCustomLink
-      href={`${currentCourseUrl}/members`}
-      displayText="Members"
-    />,
-    false && (
+    CanAccessStudent() && (
+      <MainNavBarCustomLink
+        href={`${currentCourseUrl}/student`}
+        displayText="Student"
+      />
+    ),
+    CanAccessTeacher() && (
+      <MainNavBarCustomLink
+        href={`${currentCourseUrl}/overview`}
+        displayText="Overview"
+      />
+    ),
+    CanAccessTeacher() && (
+      <MainNavBarCustomLink
+        href={`${currentCourseUrl}/attendance`}
+        displayText="Attendance"
+      />
+    ),
+    CanAccessTeacher() && (
+      <MainNavBarCustomLink
+        href={`${currentCourseUrl}/members`}
+        displayText="Members"
+      />
+    ),
+    isDevMode && (
       <MainNavBarCustomLink
         href={`${currentCourseUrl}/testing-playground`}
         displayText="Testing"
@@ -106,12 +125,20 @@ export default function MainBar() {
           </div>
           <CourseSelection />
         </div>
-        <div className="md:hidden">
-          <MobileNav NavItems={NavComponents} />
-        </div>
-        <div className="hidden md:block">
-          <MainNav NavItems={NavComponents} />
-        </div>
+        {session.status === 'loading' ? (
+          <div className="px-6">
+            <Skeleton className="w-48 h-6 " />
+          </div>
+        ) : (
+          <>
+            <div className="md:hidden">
+              <MobileNav NavItems={NavComponents} />
+            </div>
+            <div className="hidden md:block">
+              <MainNav NavItems={NavComponents} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
