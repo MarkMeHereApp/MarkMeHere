3beb3f15a712a7bffa8bcd4a48d6867aa85e29a7
Benjamin Werner <bewerner23@gmail.com>
2023-11-08 02:30:30 -0500

    Adding sheet for mobile view

--- app/(dashboard)/[organizationCode]/[courseCode]/components/main-bar.tsx
+++ app/(dashboard)/[organizationCode]/[courseCode]/components/main-bar.tsx
@@ -8,8 +8,15 @@ import Link from 'next/link';
 import { Icons } from '@/components/ui/icons';
 import { usePathname } from 'next/navigation'; // Import the useRouter hook
 import { useCourseContext } from '../context-course';
+import { useOrganizationContext } from '../../context-organization';
+import { useSession } from 'next-auth/react';
+import isDevMode from '@/utils/isDevMode';
+import { MobileNav } from './mobile-nav';
 
 export default function MainBar() {
+  const session = useSession();
+  const { organizationUrl } = useOrganizationContext();
+  const pathname = usePathname();
   const { currentCourseUrl } = useCourseContext();
   const isQRCodePage = usePathname() === `${currentCourseUrl}/qr`;
   const isVerificationPage =
@@ -20,11 +27,75 @@ export default function MainBar() {
       ? 'border-b flex-col'
       : 'border-b flex-col hover:opacity-100 opacity-0 transition-opacity duration-200 absolute w-full z-10 bg-background';
 
+  function isActive(href: string) {
+    // Since the Admin Dashboard has a bunch of pages inside of it, we have to be a big janky with it, and highlight the admin dashboard for all of its pages.
+    if (href === `${organizationUrl}/manage-courses`) {
+      return (
+        pathname === `${organizationUrl}/manage-courses` ||
+        pathname === `${organizationUrl}/manage-site-users` ||
+        pathname === `${organizationUrl}/admin-settings`
+      );
+    }
+    return pathname === href;
+  }
+
+  const getLinkClassName = (path: string) => {
+    const commonClasses = 'flex text-md font-medium pb-0';
+    const activeLinkStyles = `${commonClasses} text-primary`;
+    const inactiveLinkStyles = `${commonClasses} text-muted-foreground hover:text-primary`;
+    return isActive(path) ? activeLinkStyles : inactiveLinkStyles;
+  };
+
+  const getBorderClassName = (path: string) => {
+    const commonClasses = 'w-full p-3 pb-2 px-0';
+    const activeBorderStyles = `${commonClasses} border-primary`;
+    const inactiveBorderStyles = `${commonClasses} border-transparent hover:border-primary hover:border-solid`;
+    return isActive(path) ? activeBorderStyles : inactiveBorderStyles;
+  };
+
+  type MainNavBarCustomLinkProps = {
+    href: string;
+    displayText: string;
+  };
+
+  const MainNavBarCustomLink: React.FC<MainNavBarCustomLinkProps> = ({
+    href,
+    displayText
+  }) => (
+    <Link href={href} className={getLinkClassName(href)}>
+      <span className={getBorderClassName(href)}>{displayText}</span>
+    </Link>
+  );
+
+  const NavComponents = [
+    <MainNavBarCustomLink
+      href={`${currentCourseUrl}/student`}
+      displayText="Student"
+    />,
+    <MainNavBarCustomLink
+      href={`${currentCourseUrl}/overview`}
+      displayText="Overview"
+    />,
+    <MainNavBarCustomLink
+      href={`${currentCourseUrl}/attendance`}
+      displayText="Attendance"
+    />,
+    <MainNavBarCustomLink
+      href={`${currentCourseUrl}/members`}
+      displayText="Members"
+    />,
+    false && (
+      <MainNavBarCustomLink
+        href={`${currentCourseUrl}/testing-playground`}
+        displayText="Testing"
+      />
+    )
+  ];
   return (
     <div className={divClassName}>
       <div className="flex items-center justify-between py-2">
         <div className="flex flex-row sm:items-center ml-6 sm:space-x-2 mr-4">
-          <div className="hidden sm:flex justify-center items-center">
+          <div className="hidden lg:flex justify-center items-center">
             <Link href={`${currentCourseUrl}/overview`}>
               <Icons.logo
                 className="hover:wave-infinite"
@@ -35,18 +106,11 @@ export default function MainBar() {
           </div>
           <CourseSelection />
         </div>
-        <div className="flex align-top items-center justify-end">
-          <div className="flex pl-2">
-            <MainNav />
-          </div>
-          <Icons.line
-            className="ml-1 -mr-3"
-            style={{ width: '50px', height: '50px' }}
-          />
-          <ModeToggle />
-          <div className="pl-4 pr-8">
-            <UserNav />
-          </div>
+        <div className="md:hidden">
+          <MobileNav NavItems={NavComponents} />
+        </div>
+        <div className="hidden md:block">
+          <MainNav NavItems={NavComponents} />
         </div>
       </div>
     </div>
