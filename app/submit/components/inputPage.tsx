425adb8188cee50dc4dcab1fb9edcfcfd5c7cba6
Walshington <nicholaswalsh77@gmail.com>
2023-11-10 13:55:48 -0500

    Random format

--- app/submit/components/inputPage.tsx
+++ app/submit/components/inputPage.tsx
@@ -1,24 +1,20 @@
 'use client';
-import React, {  useEffect,useState,useRef, useMemo } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import InputTable from './inputTable';
 
 const InputPage: React.FC = () => {
-
-
   const Stars = dynamic(() => import('@/components/background/stars'), {
     ssr: false
   });
-  
-    
-  return (     
+
+  return (
     <>
       <div className="absolute top-0 right-0 h-full w-full">
         <Stars />
       </div>
       <InputTable></InputTable>
-    </>                   
-    
+    </>
   );
 };
 
