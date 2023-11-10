ecec851a1d9732b805b29f8f0d9b90d4f28539f2
Samdosi <87053736+Samdosi@users.noreply.github.com>
2023-11-10 18:37:47 -0500

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,32 +13,5 @@ export default defineConfig({
       framework: 'next',
       bundler: 'webpack'
     }
-  },
-  retries: {
-    runMode: 2 // Number of retries for the entire test run
-  },
-
-  // Configuration for environment-specific variables
-  env: {
-    baseUrl: 'http://localhost:3000/landing-page',
-    username: 'your_username',
-    password: 'your_password'
-  },
-
-  // Configuration for viewport sizes
-  viewportWidth: 1200,
-  viewportHeight: 800,
-
-  // Configuration for screenshots and videos
-  screenshotsFolder: 'cypress/screenshots',
-  videosFolder: 'cypress/videos',
-  video: true,
-
-  failOnUncaughtException: true,
-
-  ignoreTestFiles: ['**/examples/*.spec.ts'],
-
-  pluginsFile: 'cypress/plugins/index.ts',
-
-  webpackOptions: {}
+  }
 });
