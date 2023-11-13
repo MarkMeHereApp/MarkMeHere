ed0c9108d6aa122b18d1e04ae4a13d6749fcfd9b
Benjamin Werner <bewerner23@gmail.com>
2023-11-13 07:40:35 -0500

    Adding migration

---
+++ prisma/migrations/20231113124019_pre_release_v1/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Organization" ALTER COLUMN "lightTheme" SET DEFAULT 'light_blue',
+ALTER COLUMN "darkTheme" SET DEFAULT 'dark_yellow',
+ALTER COLUMN "allowUsersToUseGoogleMaps" SET DEFAULT true;
